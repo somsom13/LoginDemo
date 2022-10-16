2fedca465d3a02b5a6830bd79b7fcd16be473e5b
somsom13 <somin1113@naver.com>
2022-10-16 20:07:59 +0900

    ✏️[MOD]react cors 허용

--- src/main/java/com/example/LoginDemo/SecurityConfig.java
+++ src/main/java/com/example/LoginDemo/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.LoginDemo;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,14 +11,20 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig{
+    private final UserService userService;
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
                 .authorizeRequests()
-                .antMatchers("/","/login","/join").permitAll();
+                .mvcMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().permitAll();
 
         return http.build();
     }
